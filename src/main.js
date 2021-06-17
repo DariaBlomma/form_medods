3ff62ca11b5780aec14deefc1e8332ef65e57c32
Daria Kozina <kozina.flowers@yandex.ru>
2021-06-17 14:20:47 +0300

    deleted unnecessary commentaries and files

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
 
 import { createApp } from 'vue'
-// import Vue from 'vue'
 import App from './App.vue'
-// import Vuelidate from 'vuelidate'
 const app = createApp(App)
-// app.component('my-form', {})
-// const myForm = app.component('my-form')
 
-// import Vuelidate from 'vuelidate'
-// Vue.use(Vuelidate)
-
-// app.use(Vuelidate)
 app.mount('#app')
+app.config.devtools = true;
 
 
-// Vue.use(Vuelidate)
-// createApp(App).mount('#app')
-
-// vuelidate не работает, $v is not defined
 
